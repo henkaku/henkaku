6b135e8bbde6b80615f2fd2f50d57ed9c12897a4
Ilya Zhuravlev <whatever@xyz.is>
2016-07-05 10:47:50 -0400

    urop: store function ptr in kernel stack

--- build.sh
+++ build.sh
@@ -31,12 +31,12 @@ echo "2) Kernel ROP"
 
 echo "3) User ROP"
 $PREPROCESS urop/loader.rop.in -o build/loader.rop
-roptool -s build/loader.rop -t webkit-360-pkg -o build/loader.rop.bin >/dev/null
+roptool -s build/loader.rop -t webkit-360-pkg -o build/loader.rop.bin -v
 erb urop/stage2.rop.in > build/stage2.rop.in
 cp urop/common.rop build/
 cp urop/functions.rop build/
 $PREPROCESS build/stage2.rop.in -o build/stage2.rop
-roptool -s build/stage2.rop -t webkit-360-pkg -o build/stage2.rop.bin >/dev/null
+roptool -s build/stage2.rop -t webkit-360-pkg -o build/stage2.rop.bin -v
 
 echo "4) Webkit"
 cp webkit/exploit.html output/
