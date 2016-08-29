c41afa3e8ca41eb848790c4f2723c99f805a0cf0
Yifan Lu <me@yifanlu.com>
2016-08-28 20:31:22 -0700

    Initial release

---
+++ README.md
@@ -0,0 +1,45 @@
+This is a stripped down version of the full HENkaku build system.
+
+# Build
+
+Only Linux is supported. You will need:
+
+* roptool
+* Python 3
+* Ruby (specifically, the `erb` command-line tool)
+* Vita SDK: download from https://goo.gl/QpX5zM
+* Uglify JS (Ubuntu 16.04 package node-uglify) tested version 2.4.15
+* Other standard tools that you should already have installed, such as openssl, dd, cat, touch
+
+All tools should be in your PATH.
+
+Run `./build.sh` to build everything. This script first cleans up all build directories and then builds the exploit.
+
+# Develop
+
+It's useful to have everything be automatically rebuilt when a source file changes. To achieve that, install `entr` and execute in a separate termina:
+
+```
+while sleep 1; do find build.sh urop/ webkit/ | entr -d ./build.sh ; done
+```
+
+Then when you change a source file (or add a new one), everything will be automatically rebuilt. Note that if you add a new directory, you will need to update the script.
+
+# Distrib
+
+Distribute only the files from the `output` directory, don't distribute any other files.
+
+There are two versions of the exploit, located in the `output` directory.
+
+The `static` subdirectory contains a version that does not require any server-side scripting support. You can serve it using any web server (e.g. `python3 -m http.server`). The exploit is self contained and doesn't download any files.
+
+The `dynamic` subdirectory contains a version that has its payload split into two stages. The first stage downloads the second stage from the internet. You need to serve this directory using a server that supports PHP scripting. This version is more stable and is recommended.
+
+# Exploit
+
+To run the exploit, first cold boot your Vita, then navigate to `exploit.html`. The following situations are possible:
+
+* browser doesn't display any alert()s and displays a gray "Please wait..." screen after a few seconds: this is normal, allow up to 10 reloads before the exploit actually triggers
+* you get a "trigger" alert(), then nothing happens: the exploit succeeded
+* you get a "trigger" alert(), then the browser crashes: the exploit failed, retry it a few times, then reboot the Vita and try again
+* you get a "trigger" alert(), then the system crashes: that shouldn't happen, try the exploit again
---
+++ build.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# build: tmp build files
+# output: final files only, for distribution
+rm -rf build output
+mkdir build output
+mkdir output/static output/dynamic
+
+
+if [ -z "$1" ]; then
+	echo "Please copy sample.config.in to your.config, configure it, and then launch this script as ./build.sh your.config"
+	exit 1
+fi
+
+source $1
+
+if [ -z "$RELEASE" ] || [ -z "$PKG_URL_PREFIX" ] || [ -z "$STAGE2_URL_BASE" ]; then
+	echo "Please make sure all of the following variables are defined in your config file:"
+	echo "RELEASE, PKG_URL_PREFIX, STAGE2_URL_BASE"
+	echo "(see sample.config.in for an example)"
+	exit 2
+fi
+
+set -ex
+
+CC=arm-vita-eabi-gcc
+LD=arm-vita-eabi-gcc
+AS=arm-vita-eabi-as
+OBJCOPY=arm-vita-eabi-objcopy
+LDFLAGS="-T payload/linker.x -nodefaultlibs -nostdlib -pie"
+DEFINES="-DRELEASE=$RELEASE"
+PREPROCESS="$CC -E -P -C -w -x c $DEFINES"
+CFLAGS="-fPIE -fno-zero-initialized-in-bss -std=c99 -mcpu=cortex-a9 -Os -mthumb $DEFINES"
+
+echo "0) User payload"
+
+# skipped
+
+echo "1) Payload"
+
+cp payload/loader.enc build/loader.enc
+cp payload/payload.enc build/payload.enc
+./payload/write_pkg_url.py build/payload.enc $PKG_URL_PREFIX
+
+echo "2) Kernel ROP"
+
+cp krop/krop.rop build/krop.rop
+
+echo "3) User ROP"
+echo "symbol stage2_url_base = \"$STAGE2_URL_BASE\";" > build/config.rop
+
+./urop/make_rop_array.py build/loader.enc kx_loader build/kx_loader.rop
+./urop/make_rop_array.py build/payload.enc second_payload build/second_payload.rop
+
+$PREPROCESS urop/exploit.rop.in -o build/exploit.rop.in
+erb build/exploit.rop.in > build/exploit.rop
+roptool -s build/exploit.rop -t urop/webkit-360-pkg -o build/exploit.rop.bin >/dev/null
+
+$PREPROCESS urop/loader.rop.in -o build/loader.rop.in
+erb build/loader.rop.in > build/loader.rop
+roptool -s build/loader.rop -t urop/webkit-360-pkg -o build/loader.rop.bin >/dev/null
+
+echo "4) Webkit"
+# Static website
+$PREPROCESS webkit/exploit.js -DSTATIC=1 -o build/exploit.static.js
+uglifyjs build/exploit.static.js -m "toplevel" > build/exploit.js
+touch output/static/exploit.html
+printf "<script>" >> output/static/exploit.html
+cat build/exploit.js >> output/static/exploit.html
+printf "</script>" >> output/static/exploit.html
+./webkit/preprocess.py build/exploit.rop.bin output/static/payload.bin
+
+# Dynamic website
+$PREPROCESS webkit/exploit.js -DSTATIC=0 -o build/exploit.dynamic.js
+uglifyjs build/exploit.dynamic.js -m "toplevel" > build/exploit.js
+touch output/dynamic/exploit.html
+printf "<script src='payload.js'></script><script>" >> output/dynamic/exploit.html
+cat build/exploit.js >> output/dynamic/exploit.html
+printf "</script>" >> output/dynamic/exploit.html
+cp output/static/payload.bin output/dynamic/stage2.bin
+./webkit/preprocess.py build/loader.rop.bin output/dynamic/payload.js
+cp webkit/stage2.php output/dynamic/stage2.php
+cp webkit/stage2.go output/dynamic/stage2.go
